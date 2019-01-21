49c5bb3ea104e775c7bf64c6f005eb05c2f02c07
user <user@localhost.localdomain>
2019-01-21 17:16:43 +0530

    Adding misc files

--- temp.c
+++ temp.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
 	// printf("../..");
 	int a;
-	for(a=0;a<100;a++)
-		printf("%d ",a);
-	// scanf("%d",&a);
+        srand(10);
+	scanf("%d",&a);
+	printf("%d\n",a);
+	 //for(a=0;a<100;a++)
+	 //	printf("%d ",rand()%500);
+	// // scanf("%d",&a);
 	// printf("%d\n",a );
 	return 0;
 }
