9900c3d15b30c33a5e7d9c4a5c363acfff3e65a5
Saketh <alurusaisaketh@gmail.com>
2019-04-08 15:21:48 +0530

    Add test for read

--- Assignment 6/myprog.cpp
+++ Assignment 6/myprog.cpp
@@ -8,12 +8,17 @@ int main()
 	init();
 	string filename("file.txt");
 	int fd = my_open(filename,1);
-	string test1 = "Hello world";
+	string test1 = "Hello saketh";
 	char test[100];
 	strcpy(test,test1.c_str());
 	int bytes_written = my_write(fd,test,strlen(test));
 	cout<<"Written "<<bytes_written<<endl;
 	my_close(fd);
+	fd = my_open(filename,0);
+	char read_test[100];
+	int bytes_read = my_read(fd,read_test,100);
+	read_test[bytes_read] = '\0';
+	cout<<"Read "<<bytes_read<<" bytes, data = "<<read_test<<endl;
 	my_cat(filename);
 	cout<<endl;
 	my_copy(filename,1); 
