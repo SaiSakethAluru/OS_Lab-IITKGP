c9348efd38c391e273d85ed3e7d7186dae06f5cd
Rahul Kranti Kiran <krantikiran.68@gmail.com>
2019-04-08 15:39:43 +0530

    Added commits part 2

--- Assignment 6/myprog.cpp
+++ Assignment 6/myprog.cpp
@@ -12,23 +12,43 @@ int main()
 	cout<<"Enter memory size in Mb: ";
 	cin>>msize;
 	init(bsize,msize);
+
+	// The file in mfs 
 	string filename("file.txt");
+
+	// First open the file
 	int fd = my_open(filename,1);
 	string test1 = "Hello saketh";
 	char test[100];
 	strcpy(test,test1.c_str());
+
+	// Write the string to the file in mfs using my_write
 	int bytes_written = my_write(fd,test,strlen(test));
 	cout<<"Written "<<bytes_written<<endl;
+
+	// Close the file
 	my_close(fd);
+
+	// Open the same file in read mode
 	fd = my_open(filename,0);
 	char read_test[100];
+
+	// read from the file
 	int bytes_read = my_read(fd,read_test,100);
 	read_test[bytes_read] = '\0';
 	cout<<"Read "<<bytes_read<<" bytes, data = "<<read_test<<endl;
+
+	// Print the entire file to stdout using my_cat
 	my_cat(filename);
 	cout<<endl;
+
+	// Copy file in mfs to linus fs
 	my_copy(filename,1); 
 	strcpy(test,"test.txt");
+
+	// copy test file from linux fs to mfs
 	my_copy("test.txt",0);
+
+	// Print the newly copied file
 	my_cat("test.txt");
 }
